649775ffcd8229e47b373d3e5869a41ff6afc693
3labveb <85859545+3labveb@users.noreply.github.com>
2021-06-25 00:13:45 +0700

    Update main.py

--- main.py
+++ main.py
@@ -1,51 +1,83 @@
-# Подключение библиотек для работы с Flask и Blueprint
-from flask import Flask, jsonify, Blueprint
-# Подключение библиотеки для создания автоматической документации api
-from flasgger import Swagger
-# Подключение части нашего веб-сервиса с использованием Blueprint
-from sitepart.sitepart import sitepart
-# Приложение Flask
-app = Flask(__name__)
-# Инициализация для нашего api сервиса документации Swagger
-swagger = Swagger(app)
-# Создаем основной Blueprint сайта
-main = Blueprint("/", __name__, template_folder='templates',static_folder='static')
-# объявляем декоратор для метода http get
-# Информация, которая будет выдаваться по url /info/something
-# Параметр в <> при вводе url будет передан в переменную about функции info
-@main.route('/info/<about>/')
-def info(about):
- """Example endpoint returning about info
- This is using docstrings for specifications.
- ---
- parameters:
- - name: about
- in: path
- type: string
- enum: ['all','version', 'author', 'year']
- required: true
- default: all
- definitions:
- About:
-  type: string
- responses:
- 200:
- description: A string
- schema:
- $ref: '#/definitions/About'
- examples:
- version: '1.0'
- """
- all_info = {
- 'all': 'main_author 1.0 2020',
- 'version': '1.0',
- 'author': 'main_author',
- 'year': '2020'
- }
- result = {about:all_info[about]}
- return jsonify(result)
-# Регистрируем основной Blueprint и Blueprint другой части сайта
-app.register_blueprint(main,url_prefix='/')
-# url_prefix указывает url в контексте которого будет доступна часть данного Blueprint
-app.register_blueprint(sitepart,url_prefix='/sitepart')
+from flask import Flask, Blueprint
+from flask_restplus import Api, Resource
+app = Flask(__name__)
+api = Api(app = app)
+# описание главного блока нашего api http://127.0.0.1:5000/main/.
+name_space = api.namespace('main', description='Main APIs')
+@name_space.route("/")
+class MainClass(Resource):
+    def get(self):
+        return {"status": "Got new data"}
+    def post(self):
+        return {"status": "Posted new data"}
+# подключение api из другого файла
+from part.part import api as partns1
+api.add_namespace(partns1)
+from part.parttmpl import api as partns2
+from part.parttmpl import templ as templ
+api.add_namespace(partns2)
+app.register_blueprint(templ,url_prefix='/templ')
+
+from flask_restplus import fields
+# определение модели данных массива
+list_ = api.model('list', {
+        'len': fields.String(required=True, description='Size of array'),
+    'array': fields.List(fields.String,required=True, description='Some array'),
+})
+
+# массив, который хранится в оперативной памяти
+allarray = ['1']
+name_space1 = api.namespace('list', description='list APIs')
+@name_space1.route("/")
+class ListClass(Resource):
+    @name_space1.doc("")
+    @name_space1.marshal_with(list_)
+    def get(self):
+        """"Получение всего хранимого массива"""
+        return {'len': str(len(allarray)), 'array': allarray}
+    @name_space1.doc("")
+    # ожидаем на входе данных в соответствии с моделью list_
+    @name_space1.expect(list_)
+    # маршалинг данных в соответствии с list_
+    @name_space1.marshal_with(list_)
+    def post(self):
+        """Создание массива/наше описание функции пост"""
+        global allarray
+        # получить переданный массив из тела запроса
+        allarray = api.payload['array']
+         # возвратить новый созданный массив клиенту
+        return {'len': str(len(allarray)), 'array': allarray}
+# модель данные с двумя параметрами строкового типа
+minmax = api.model('minmax', {'min':fields.String, 'max':fields.String}, required=True, description='two values')
+# url 127.0.0.1/list/mimmax
+@name_space1.route("/minmax")
+class MinMaxClass(Resource):
+    @name_space1.doc("")
+    # маршаллинг данных в соответствии с моделью minmax
+    @name_space1.marshal_with(minmax)
+    def get(self):
+        """Получение Максимума и Минимума массива"""
+        global allarray
+        return {'min': min(allarray), 'max': max(allarray)}
+api.add_namespace(name_space1)
+
+from flask_restplus import reqparse
+from random import random
+reqp = reqparse.RequestParser()
+# добавление аргументов передаваемых запросом GET
+# например GET http://127.0.0.1:5000/list/makerand?len=7&minval=1&maxval=12
+reqp.add_argument('len', type=int, required=False)
+reqp.add_argument('minval', type=float, required=False)
+reqp.add_argument('maxval', type=float, required=False)
+@name_space1.route("/makerand")
+class MakeArrayClass(Resource):
+    @name_space1.doc("")
+    # маршаллинг данных в соответствии с моделью minmax
+    @name_space1.expect(reqp)
+    @name_space1.marshal_with(list_)
+    def get(self):
+        """Возвращение массива случайных значений от min до max"""
+        args = reqp.parse_args()
+        array = [random()*(args['maxval']-args['minval'])+args['minval'] for i in range(args['len'])]
+        return {'len': args['len'], 'array': array}
 app.run(debug=True,host='127.0.0.1',port=5000)
