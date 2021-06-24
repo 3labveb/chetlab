de3bef5c261375f00c1bd49d5054f81407522b7c
3labveb <85859545+3labveb@users.noreply.github.com>
2021-06-24 20:34:29 +0700

    Update idz.py

--- idz.py
+++ idz.py
@@ -1,228 +1,274 @@
-from flask_restplus import Api, Resource
-from flask import Flask, Blueprint
-app = Flask(__name__)
-api = Api(app = app)
-# описание главного блока нашего api http://127.0.0.1:5000/main/.
-name_space = api.namespace('main', description='Main APIs')
+from flask import Flask, Blueprint
+from flask_restplus import Api, Resource
+app = Flask(__name__)
+api = Api(app = app)
+# описание главного блока нашего api http://127.0.0.1:5000/main/.
+name_space = api.namespace('main', description='Main APIs')
 @name_space.route("/")
-class MainClass(Resource):
-    def get(self):
-        return {"status": "Got new data"}
-    def post(self):
-        return {"status": "Posted new data"}
+class MainClass(Resource):
+    def get(self):
+        return {"status": "Got new data"}
+    def post(self):
+        return {"status": "Posted new data"}
+    def patch(self):
+        return {"status": "Patched new data"}
+    def put(self):
+        return {"status": "Put new data"}
 
-from flask_restplus import fields
-# определение модели данных массива
-list_ = api.model('list', {
-    'id': fields.Integer(required=True, description='id film'),
-    'author': fields.String(required=True, description='author film'),
-    'name': fields.String(required=True, description='name film'),
-    'time': fields.Float(required=True, description='time film'),
-    'score': fields.Float(required=True, description='score film'),
-    'array': fields.List(fields.Raw,required=True, description='all list')
+from flask_restplus import fields
+# определение модели данных массива
+list_ = api.model('list', {
+    'id': fields.Integer(required=True, description='disease id'),
+    'disease': fields.String(required=True, description='disease'),
+    'country': fields.String(required=True, description='country'),
+    'recovered': fields.Integer(required=True, description='number of recovered'),
+    'dead': fields.Integer(required=True, description='number of deaths'), 
+    'arr': fields.List(fields.Raw,required=True, description='all list'),
 })
 
-# массив, который хранится в оперативной памяти
-ls=[{"id": 0, "author":"K.Tarantino", "name":"kill bill 1", "time":112, "score": 9.8}]
+# массив, который хранится в оперативной памяти
+ls=[{"id": 0, "disease":"COVID-19", "country":"China", "recovered":100, "dead":3}]
 universalID=int(0)
-allarray = ls
-name_space1 = api.namespace('list', description='list APIs')
-@name_space1.route("/")
-class ListClass(Resource):
-    @name_space1.doc("")
+allarray = ls
+name_space1 = api.namespace('list', description='list APIs')
+
+@name_space1.route("/ListClass")
+class ListClass(Resource):
+    @name_space1.doc("")
     @name_space1.marshal_with(list_)
-    def get(self):
-        """"Получение всего хранимого массива"""
-        return { 'array': ls}
-    @name_space1.doc("")
-    # ожидаем на входе данных в соответствии с моделью list_
-    @name_space1.expect(list_)
-    # маршалинг данных в соответствии с list_
-    @name_space1.marshal_with(list_)
-    def post(self):
-        """Создание массива/наше описание функции пост"""
-        global allarray
-        # получить переданный массив из тела запроса
-        
-        film={"id":api.payload['id'], "author": api.payload['author'], "name": api.payload['name'], "time": api.payload['time'], "score": api.payload['score'] } 
+    def get(self):
+        """Получение всего хранимого массива"""
+        global ls
+        return ls
+    @name_space1.doc("")
+    # ожидаем на входе данных в соответствии с моделью list_
+    @name_space1.expect(list_)
+    # маршалинг данных в соответствии с list_
+    @name_space1.marshal_with(list_)
+    def post(self):
+        """Создание массива"""
+        global allarray
+        # получить переданный массив из тела запроса
+        
+        sick={"id":api.payload['id'], "disease": api.payload['disease'], "country": api.payload['country'], "recovered": api.payload['recovered'], "dead": api.payload['dead']} 
 
-        ls.append(film)
-        # возвратить новый созданный массив клиенту
-        return { 'array': ls}
-# модель данные с двумя параметрами строкового типа
-sortsc = api.model('lst', { 'array':fields.List(fields.Raw,required=True, description='all list')})
-# url 127.0.0.1/list/mimmax
-@name_space1.route("/getsortScore")
-class getsortScore(Resource):
-    @name_space1.doc("")
-    # маршаллинг данных в соответствии с моделью minmax
-    @name_space1.marshal_with(sortsc)
-    def get(self):
-        """Получение сортировки по оценке"""
-        global ls
-        sor=sorted(ls,key=lambda film: film['score'])
-        return {'array': sor}
-
-@name_space1.route("/getsorttime")
-class getsortTime(Resource):
-    @name_space1.doc("")
-    # маршаллинг данных в соответствии с моделью minmax
-    @name_space1.marshal_with(sortsc)
-    def get(self):
-        """Получение сортировки по времени"""
-        global ls
-        tim=sorted(ls,key=lambda film: film['time'])
-        return {'array': tim}
-
-@name_space1.route("/getsortauthor")
-class getsortAuthor(Resource):
-    @name_space1.doc("")
-    # маршаллинг данных в соответствии с моделью minmax
+        ls.append(sick)
+        # возвратить новый созданный массив клиенту
+        return { 'array': ls}
+# модель данные с двумя параметрами строкового типа
+sortsc = api.model('lst', { 'array':fields.List(fields.Raw,required=True, description='all list')})
+# url 127.0.0.1/list/mimmax
+@name_space1.route("/getsortDisease")
+class getsortDisease(Resource):
+    @name_space1.doc("")
+    # маршаллинг данных в соответствии с моделью minmax
     @name_space1.marshal_with(sortsc)
-    def get(self):
-        """Получение сортировки по автору"""
-        global ls
-        aut=sorted(ls,key=lambda film: film['author'])
-        return {'array': aut}
+    def get(self):
+        """Получение сортировки по заболеванию"""
+        global ls
+        dis=sorted(ls,key=lambda sick: sick['disease'])
+        return {'array': dis}
 
-@name_space1.route("/getsortname")
-class getsortName(Resource):
-    @name_space1.doc("")
-    # маршаллинг данных в соответствии с моделью minmax
-    @name_space1.marshal_with(sortsc)
-    def get(self):
-        """Получение сортировки по названию"""
-        global ls
-        nam=sorted(ls,key=lambda film: film['name'])
-        return {'array': nam}
+@name_space1.route("/getsortCountry")
+class getsortCountry(Resource):
+    @name_space1.doc("")
+    # маршаллинг данных в соответствии с моделью minmax
+    @name_space1.marshal_with(sortsc)
+    def get(self):
+        """Получение сортировки по странам"""
+        global ls
+        cou=sorted(ls,key=lambda sick: sick['country'])
+        return {'array': cou}
 
-@name_space1.route("/getsortid")
-class getsortId(Resource):
-    @name_space1.doc("")
-    # маршаллинг данных в соответствии с моделью minmax
-    @name_space1.marshal_with(sortsc)
-    def get(self):
-        """Получение сортировки по id"""
-        global ls
-        ide=sorted(ls,key=lambda film: film['id'])
-        return {'array': ide}
-oneval=api.model('one', {'val':fields.String}, required=True, description='one values')
+@name_space1.route("/getsortRecovered")
+class getsortRecovered(Resource):
+    @name_space1.doc("")
+    # маршаллинг данных в соответствии с моделью minmax
+    @name_space1.marshal_with(sortsc)
+    def get(self):
+        """Получение сортировки по выздоровевшим"""
+        global ls
+        rec=sorted(ls,key=lambda sick: sick['recovered'])
+        return {'array': rec}
 
-@name_space1.route("/getmaxscore")
-class getmaxscore(Resource):
-    @name_space1.doc("")
-    # маршаллинг данных в соответствии с моделью minmax
-    @name_space1.marshal_with(oneval)
-    def get(self):
-        """Получение максимального по очкам"""
-        global ls
-        mx=max([film['score'] for film in ls ])
-        return {'val': mx}
+@name_space1.route("/getsortDead")
+class getsortDead(Resource):
+    @name_space1.doc("")
+    # маршаллинг данных в соответствии с моделью minmax
+    @name_space1.marshal_with(sortsc)
+    def get(self):
+        """Получение сортировки по умершим"""
+        global ls
+        dea=sorted(ls,key=lambda sick: sick['dead'])
+        return {'array': dea}
 
-@name_space1.route("/getmaxtime")
-class getmaxtime(Resource):
-    @name_space1.doc("")
-    # маршаллинг данных в соответствии с моделью minmax
-    @name_space1.marshal_with(oneval)
-    def get(self):
-        """Получение максимального по времени"""
-        global ls
-        mx=max([film['time'] for film in ls ])
-        return {'val': mx}
+@name_space1.route("/getsortId")
+class getsortId(Resource):
+    @name_space1.doc("")
+    # маршаллинг данных в соответствии с моделью minmax
+    @name_space1.marshal_with(sortsc)
+    def get(self):
+        """Получение сортировки по id"""
+        global ls
+        idi=sorted(ls,key=lambda sick: sick['id'])
+        return {'array': idi}
+oneval=api.model('one', {'val':fields.String}, required=True, description='one values')
 
-@name_space1.route("/getminscore")
-class getminscore(Resource):
-    @name_space1.doc("")
-    # маршаллинг данных в соответствии с моделью minmax
-    @name_space1.marshal_with(oneval)
-    def get(self):
-        """Получение минимального по оценке"""
-        global ls
-        mn=min([film['score'] for film in ls ])
-        return {'val': mn}
+#MAX
+@name_space1.route("/getmaxDead")
+class getmaxDead(Resource):
+    @name_space1.doc("")
+    # маршаллинг данных в соответствии с моделью minmax
+    @name_space1.marshal_with(oneval)
+    def get(self):
+        """Получение максимального по умершим"""
+        global ls
+        mx=max([sick['dead'] for sick in ls ])
+        return {'val': mx}
 
-@name_space1.route("/getmintime")
-class getmintime(Resource):
-    @name_space1.doc("")
-    # маршаллинг данных в соответствии с моделью minmax
-    @name_space1.marshal_with(oneval)
-    def get(self):
-        """Получение минимального по времени"""
-        global ls
-        mn=min([film['time'] for film in ls ])
-        return {'val': mn}
+@name_space1.route("/getmaxRecovered")
+class getmaxRecovered(Resource):
+    @name_space1.doc("")
+    # маршаллинг данных в соответствии с моделью minmax
+    @name_space1.marshal_with(oneval)
+    def get(self):
+        """Получение максимального по выздоровевшим"""
+        global ls
+        mx=max([sick['recovered'] for sick in ls ])
+        return {'val': mx}
 
-@name_space1.route("/getsredscore")
-class getsredscore(Resource):
-    @name_space1.doc("")
-    # маршаллинг данных в соответствии с моделью minmax
-    @name_space1.marshal_with(oneval)
-    def get(self):
-        """Получение среднего по оценке"""
-        global ls
-        srd=sum([film['score'] for film in ls ])/len(ls)
-        return {'val': srd}
+#AVERAGE
+@name_space1.route("/getaverDead")
+class getaverDead(Resource):
+    @name_space1.doc("")
+    # маршаллинг данных в соответствии с моделью minmax
+    @name_space1.marshal_with(oneval)
+    def get(self):
+        """Получение среднего по умершим"""
+        global ls
+        aver=sum([sick['dead'] for sick in ls ])/len(ls)
+        return {'val': aver}
 
-@name_space1.route("/getsredtime")
-class getsredtime(Resource):
-    @name_space1.doc("")
-    # маршаллинг данных в соответствии с моделью minmax
-    @name_space1.marshal_with(oneval)
-    def get(self):
-        """Получение среднего по времени"""
-        global ls
-        srd=sum([film['time'] for film in ls ])/len(ls)
-        return {'val': srd}
+@name_space1.route("/getaverRecovered")
+class getaverRecovered(Resource):
+    @name_space1.doc("")
+    # маршаллинг данных в соответствии с моделью minmax
+    @name_space1.marshal_with(oneval)
+    def get(self):
+        """Получение среднего по выздоровевшим"""
+        global ls
+        aver=sum([sick['recovered'] for sick in ls ])/len(ls)
+        return {'val': aver}
 
-@name_space1.route("/getsredtime")
-class getsortScore(Resource):
-    @name_space1.doc("")
-    # маршаллинг данных в соответствии с моделью minmax
-    @name_space1.marshal_with(oneval)
-    def get(self):
-        """Получение сортировки по id"""
-        global ls
-        srd=sum([film['time'] for film in ls ])/len(ls)
-        return {'val': srd}
+#MIN
+@name_space1.route("/getminDead")
+class getminDead(Resource):
+    @name_space1.doc("")
+    # маршаллинг данных в соответствии с моделью minmax
+    @name_space1.marshal_with(oneval)
+    def get(self):
+        """Получение минимального по умершим"""
+        global ls
+        mn=min([sick['dead'] for sick in ls ])
+        return {'val': mn}
 
+@name_space1.route("/getminRecovered")
+class getminRecovered(Resource):
+    @name_space1.doc("")
+    # маршаллинг данных в соответствии с моделью minmax
+    @name_space1.marshal_with(oneval)
+    def get(self):
+        """Получение минимального по выздоровевшим"""
+        global ls
+        mn=min([sick['recovered'] for sick in ls ])
+        return {'val': mn}
+
+#REMOVING MAXIMUM DEATH
+@name_space1.route("/deletemaxDead")
+class deletemaxDead(Resource):
+    @name_space1.doc("")
+    @name_space1.marshal_with(list_)
+    def get(self):
+        """Удаление страны с максимальным количеством умерших"""
+        global ls
+        mx=max([sick['dead'] for sick in ls ])
+        ls=[sick for sick in ls if sick['dead']!=mx]
+        return { 'array': ls}
+      
+#STATISTICS
+@name_space1.route("/changeStatistic")
+class changeStatistic(Resource):
+    @name_space1.doc("")
+    # ожидаем на входе данных в соответствии с моделью list_
+    @name_space1.expect(list_)
+    # маршалинг данных в соответствии с list_
+    @name_space1.marshal_with(list_)
+    def patch(self):
+        """Изменение статистики по заболеваниям"""
+        global ls
+        aver=sum([sick['recovered'] for sick in ls ])/len(ls)
+        for sick in ls:
+          if(sick["recovered"] >= aver):
+            temp=sick["recovered"]/100*10
+            sick["recovered"]=temp+sick["recovered"]
+        return { 'array': ls}
+    
+#CHANGE COUNTRY DATA
+@name_space1.route("/changedataCountry")
+class changedataCountry(Resource):
+    @name_space1.doc("")
+    # ожидаем на входе данных в соответствии с моделью list_
+    @name_space1.expect(list_)
+    # маршалинг данных в соответствии с list_
+    @name_space1.marshal_with(list_)
+    def put(self):
+        """Изменение данных по стране"""
+        global ls
+        for sick in ls:
+          if(api.payload['country'] == sick["country"]):
+            sick["id"] = api.payload['id']
+            sick["disease"] = api.payload['disease']
+            sick["recovered"] = api.payload['recovered']
+            sick["dead"] = api.payload['dead']
+        return { 'array': ls}
+    
 api.add_namespace(name_space1)
 
-from flask_restplus import reqparse
-from random import random
+from flask_restplus import reqparse
+from random import random
 
-reqp = reqparse.RequestParser()
-reqp.add_argument('id', type=int, required=False)
+reqp = reqparse.RequestParser()
+reqp.add_argument('id', type=int, required=False)
 
-@name_space1.route("/izmdfilm")
-class IzmfilClass(Resource):
-    @name_space1.doc("")
-    # маршаллинг данных в соответствии с моделью minmax
-    @name_space1.expect(reqp)
-    @name_space1.marshal_with(list_)
-    def get(self):
-        """удаление фильма по ID"""
-        global ls
-        args = reqp.parse_args()
-        ls=[film for film in ls if film['id']!=args['id']]
-        return { 'array': ls}
-    @name_space1.doc("")
-    # ожидаем на входе данных в соответствии с моделью list_
-    @name_space1.expect(list_)
-    # маршалинг данных в соответствии с list_
-    @name_space1.marshal_with(list_)
-    def post(self):
-        """Изменение фильма по id"""
-        global ls
-        for film in ls:
-          if(api.payload['id'] == film["id"]):
-                film["author"] = api.payload['author']
-                film["name"] = api.payload['name']
-                film["time"] = api.payload['time']
-                film["score"] = api.payload['score']
-                return { 'array': ls}
-        
-        film={"id":api.payload['id'], "author": api.payload['author'], "name": api.payload['name'], "time": api.payload['time'], "score": api.payload['score'] } 
-        ls.append(film)
-        return ls
-app.run(debug=True,host='127.0.0.1',port=5000)
-    
+@name_space1.route("/chahgeDisease")
+class chahgeDiseaseClass(Resource):
+    @name_space1.doc("")
+    # маршаллинг данных в соответствии с моделью reqp
+    @name_space1.expect(reqp)
+    @name_space1.marshal_with(list_)
+    def get(self):
+        """Удаление заболевания по id"""
+        global ls
+        args = reqp.parse_args()
+        ls=[sick for sick in ls if sick['id']!=args['id']]
+        return { 'array': ls}
+    @name_space1.doc("")
+    # ожидаем на входе данных в соответствии с моделью list_
+    @name_space1.expect(list_)
+    # маршалинг данных в соответствии с list_
+    @name_space1.marshal_with(list_)
+    def post(self):
+        """Изменение заболевания по id"""
+        global ls
+        for sick in ls:
+          if(api.payload['id'] == sick["id"]):
+                sick["disease"] = api.payload['disease']
+                sick["country"] = api.payload['country']
+                sick["recovered"] = api.payload['recovered']
+                sick["dead"] = api.payload['dead']
+                return { 'array': ls}
+        
+        sick={"id":api.payload['id'], "disease": api.payload['disease'], "country": api.payload['country'], "recovered": api.payload['recovered'], "dead": api.payload['dead'] } 
+        ls.append(sick)
+        return ls
+app.run(debug=True,host='127.0.0.1',port=5000)   
