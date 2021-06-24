5e95cc079be73d26e837dc99867b6d95a09c1711
3labveb <85859545+3labveb@users.noreply.github.com>
2021-06-24 20:19:06 +0700

    Update idz.py

--- idz.py
+++ idz.py
@@ -1,5 +1,5 @@
-from flask_restplus import Api, Resource
 from flask import Flask, Blueprint
+from flask_restplus import Api, Resource
 app = Flask(__name__)
 api = Api(app = app)
 # описание главного блока нашего api http://127.0.0.1:5000/main/.
